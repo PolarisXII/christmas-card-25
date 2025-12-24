eb8692b84998b855a5e948a3085e0f84c8f875fe
Sylvia Wang <sylviamiaowang@gmail.com>
2025-12-25 04:12:50 +0800

    message back

---
+++ src/message.ts
@@ -0,0 +1,13 @@
+const message = `
+
+Dear Lesley,
+
+The Intergalactic Envoys of Mars (pictured on the front cover) and I wish you an epically merry Christmas. Reconnecting with you has been one of the greatest highlights of my year, and I'm super glad that we've been able to plan world domination together. I hope you have the best of luck on your startup idea, a great summer with NORMAL temporatures and lots of allergy-free cat cuddles. 
+
+Looking forward to seeing you again when I'm back in Sydney!
+
+Yours sincerely,
+Sylvia
+`
+
+export default message;
